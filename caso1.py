0300e3b5d997bb2f7255c160c30125ed360f3259
Jeriko34 <goldplayer1207@gmail.com>
2026-05-25 18:30:35 -0500

    Primer commit.

---
+++ caso1.py
@@ -0,0 +1,25 @@
+import random
+
+def min_max_m3(lst, x=0):
+    if x == len(lst):
+        return float('inf'), float('-inf') 
+    else:
+        min_r, max_r = min_max_m3(lst, x+1)
+        val = lst[x]
+        
+        if val % 3 == 0:
+            return min(val, min_r), max(val, max_r)
+        else:
+            return min_r, max_r
+
+n = int(input("Digitar tamaño de arreglo:  "))
+arreglo = [random.randint(10, 9999) for _ in range(n)]
+print(f"Arreglo: {arreglo}")
+
+min, max = min_max_m3(arreglo)
+
+if min == float('inf'):
+    print("Sin múltiplos de 3 en el arreglo. Inténtelo de nuevo.")
+else:
+    promedio = (min + max) / 2
+    print(f"Resultado = ({max}+{min})/2 = {promedio}")
